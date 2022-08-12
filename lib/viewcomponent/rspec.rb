a5bf312095a957ed595282eeabb3cae6755603a3
Simon Fish <si@mon.fish>
2022-08-12 20:33:24 +0100

    Initial commit

---
+++ lib/viewcomponent/rspec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "rspec/version"
+
+module Viewcomponent
+  module Rspec
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
